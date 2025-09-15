39e4fa97dfb6a7f6295731d61952eac9e7bc72f8
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-09-15 17:35:16 +0530

    Round 2 for the schools

--- send_mails.py
+++ send_mails.py
@@ -52,7 +52,7 @@ total_time = 0
 count = 0
 
 filepath = os.path.abspath(
-  os.path.join(os.path.dirname(__file__), 'data', 'schools.json')
+  os.path.join(os.path.dirname(__file__), 'data', 'schools2.json')
 )
 
 with open(filepath, 'r') as list:
