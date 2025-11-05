378771b58653da7df0db84b21b87bd0667e8c3c6
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-11-06 02:29:38 +0530

    Changing subject name before sending the certificates

--- send_mails.py
+++ send_mails.py
@@ -114,7 +114,7 @@ with open(filepath, 'r') as list:
 
 for recipient in recipients:
   try:
-    subject = "Thank You for Attending ISNASD’25 – Participation Certificate Attached (Test 2)"
+    subject = "Thank You for Attending ISNASD’25 – Participation Certificate Attached"
     body = f"""
     <!DOCTYPE html>
     <html>
